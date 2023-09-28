9b4a62c064fa30575e4870c5cc534e5fc405bc61
Ryan Kline <ryanjkline@protonmail.com>
2023-09-27 20:10:21 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavComponent from '@/components/NavComponent.vue'
+</script>
+
+<template>
+  <NavComponent />
+  <RouterView />
+</template>
+
+<style >
+
+</style>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <RouterLink class="navbar-brand" :to="{name: 'home'}">
+            <div class="name">Ryan Kline</div>
+            <hr class="name-spacer">
+            <div class="subtitle">Software Engineer</div>
+        </RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto mx-5">
+            <li class="nav-item mx-3">
+                <RouterLink :to="{name: 'home'}" class="nav-link">Home</RouterLink>
+            </li>
+            <li class="nav-item mx-3">
+                <RouterLink :to="{name: 'home'}" class="nav-link">About</RouterLink>
+            </li>
+            <li class="nav-item mx-3">
+                <RouterLink :to="{name: 'home'}" class="nav-link">Projects</RouterLink>
+            </li>
+            <li class="nav-item mx-3">
+                <RouterLink :to="{name: 'home'}" class="nav-link">Contact</RouterLink>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
+</template>
+
+<style>
+.navbar {
+    position: fixed !important;
+    top: 0;
+    width: 100%;
+    left: 0;
+    }
+
+.nav-link:hover, .navbar-brand {
+    background-color: inherit !important;
+}
+
+.name {
+    font-size: 1.5em;
+    text-align: center;
+}
+
+.name-spacer{
+    padding: 0;
+    margin: .4rem;
+}
+
+.subtitle {
+    font-size: 1rem;
+    text-align: center;
+}
+
+</style>
