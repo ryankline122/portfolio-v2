76c7062f3a991aa969cdca5ddfa8a4a83bf77e47
Ryan Kline <ryanjkline@protonmail.com>
2023-09-30 09:45:21 -0400

    Adds footer icons

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,13 +5,27 @@
 <template>
 <!-- Footer -->
 <footer class="bg-dark text-light p-4">
-    <div class="container">
+    <div class="container text-center">
         <div class="row">
             <div class="col-md-12 text-center">
-                <a href="https://www.linkedin.com/in/your-linkedin-profile" class="mx-3" target="_blank">LinkedIn</a>
-                <a href="https://github.com/your-github-profile" class="mx-3" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank">
+                    <img src="@/assets/linkedin.png" alt="LinkedIn" class="mx-3" />
+                </a>
+                <a href="https://github.com/your-github-profile" target="_blank">
+                    <img src="@/assets/gh.png" alt="GitHub" class="mx-3" />
+                </a>
             </div>
         </div>
     </div>
 </footer>
+
 </template>
+
+<style scoped>
+
+img {
+    max-width: 6rem;
+    height: auto;
+}
+
+</style>
