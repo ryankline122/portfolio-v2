b57cf852ec3c0ba8e3933e118c03df30487a2605
Ryan Kline <ryanjkline@protonmail.com>
2023-12-13 13:10:01 -0500

    Fixes mobile nav

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   isNavOpen: boolean = false;
+
+  toggleNav(): void {
+    if (this.isNavOpen) {
+      this.isNavOpen = false;
+    } else {
+      this.isNavOpen = true;
+    }
+  }
 }
